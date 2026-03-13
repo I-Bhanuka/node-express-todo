9c71d2d6d4679fa458101401e9b4b3cb9433951f
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-03-13 11:47:30 +0530

    Add POST route to extract form data and log it into the console

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 // Serve static files from the "public" directory when requested with the "/static" path
 app.use("/static", express.static("public"));
 
+// Middleware to parse URL-encoded data from the request body
+app.use(express.urlencoded({ extended: true }));
+
 // Set EJS as the view engine for rendering templates
 app.set("view engine", "ejs");
 
@@ -15,6 +18,11 @@ app.get('/', (req, res) => {
     // Render the "todo.ejs" template when the root URL is accessed 
     res.render('todo.ejs');
 });
+
+// Define a POST route to handle form submissions from the root URL
+app.post('/', (req, res) => {
+    console.log(req.body); // Log the form data to the console
+})
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
